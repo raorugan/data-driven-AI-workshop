3436d2ac2d54705c82e526e52f6fca884a110a46
Anthony Shaw <anthony.p.shaw@gmail.com>
2024-09-09 19:23:02 +1000

    Add support for comparing embedding models

--- src/api/backends/local.py
+++ src/api/backends/local.py
@@ -12,7 +12,7 @@ import sqlite3
 import json
 import logging
 import numpy as np
-from .models import Product
+from .models import ProductWithSimilarity
 
 HAS_FTS5 = False
 SIMILARITY_THRESHOLD = 0.77
@@ -56,7 +56,9 @@ def connect(database = 'dev.db') -> "sqlite3.Connection":
                         description text,
                         image text,
                         price real,
-                        embedding text);""")
+                        embedding text,
+                        embedding_large text);""")
+        logging.info("Created products table")
         
         if HAS_FTS5:
             # Create a FTS5 virtual table for full-text search
@@ -67,23 +69,24 @@ def connect(database = 'dev.db') -> "sqlite3.Connection":
     with open('data/test.json') as f:
         data = json.load(f)
         for product in data:
-            conn.execute("INSERT INTO products (name, description, image, price, embedding) VALUES (?, ?, ?, ?, ?)", 
+            conn.execute("INSERT INTO products (name, description, image, price, embedding, embedding_large) VALUES (?, ?, ?, ?, ?, ?)", 
                          (product['name'], 
                           product['description'], 
                           product['image'], 
                           product['price'], 
                           # Convert the embedding into a string of CSV values, this is hugely inefficient but we have 9 products
-                          ','.join([str(f) for f in product['embedding']])
+                          ','.join([str(f) for f in product.get('embedding', [])]),
+                          ','.join([str(f) for f in product.get('embedding_large', [])])
                           ))
         logging.info("Loaded test data into database")
 
     return conn
 
-def search_products(query: str, fts_query: str, embedding: list[float]) -> list[Product]:
+def search_products(query: str, fts_query: str, embedding: list[float], embedding_field: str) -> list[ProductWithSimilarity]:
     cursor = connect(':memory:').cursor()
     
     # Do a vector search. This is sqlite and we don't have a vector index, so do a similarity on ALL of them
-    cursor.execute("SELECT id, name, description, price, image, embedding FROM products");
+    cursor.execute(f"SELECT id, name, description, price, image, {embedding_field} FROM products");
     results = cursor.fetchall()
     distances: tuple[float, tuple[int, str, str, str]] = []
     for result in results:
@@ -100,7 +103,6 @@ def search_products(query: str, fts_query: str, embedding: list[float]) -> list[
     logging.info(f"Found {len(distances)} results with similarity > {SIMILARITY_THRESHOLD}")
 
     # Search the productFtsIndex table for the query
-    cursor = sqlite3.connect('dev.db').cursor()
     if HAS_FTS5:
         cursor.execute("SELECT id, name, description, price, image FROM productFtsIndex WHERE productFtsIndex MATCH ?", (fts_query,))
     else:
@@ -112,12 +114,12 @@ def search_products(query: str, fts_query: str, embedding: list[float]) -> list[
     results = []
     found_ids = []
     for product in fts_results:
-        results.append(Product(id=product[0], name=product[1], description=product[2], price=product[3], image=product[4], embedding=None))
+        results.append(ProductWithSimilarity(id=product[0], name=product[1], description=product[2], price=product[3], image=product[4], embedding=None, similarity=1))
         found_ids.append(product[0])
 
-    for _, product in distances:
+    for similarity, product in distances:
         if product[0] not in found_ids:
-            results.append(Product(id=product[0], name=product[1], description=product[2], price=product[3], image=product[4], embedding=None))
+            results.append(ProductWithSimilarity(id=product[0], name=product[1], description=product[2], price=product[3], image=product[4], embedding=None, similarity=similarity))
 
     return results
     
--- src/api/function_app.py
+++ src/api/function_app.py
@@ -1,20 +1,20 @@
+from typing import Optional
 import azure.functions as func
 import logging
 import json
 
-
 from azure.identity import AzureCliCredential, get_bearer_token_provider
-from openai import AzureOpenAI
+from openai import NOT_GIVEN, AzureOpenAI, NotGiven
 import os
 
 client: AzureOpenAI
 DEVELOPMENT = os.getenv("DEVELOPMENT", True)
 
-if not DEVELOPMENT:
+if os.getenv("AZURE_OPENAI_ENDPOINT") and os.getenv("AZURE_OPENAI_KEY"):
     client = AzureOpenAI(
         api_version="2024-02-15-preview",
         azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_key=os.getenv("AZURE_OPENAI_APIKEY")
+        api_key=os.getenv("AZURE_OPENAI_KEY")
     )
 else:
     azure_credential = AzureCliCredential(tenant_id=os.getenv("AZURE_TENANT_ID"))
@@ -25,7 +25,9 @@ else:
         azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
         azure_ad_token_provider=token_provider
     )
+
 completions_deployment = os.getenv("CHAT_DEPLOYMENT_NAME", "gpt-35-turbo")
+embeddings_deployment = os.getenv("EMBEDDINGS_DEPLOYMENT_NAME", "text-embedding-ada-002")
 
 if DEVELOPMENT:
     from backends.local import search_products
@@ -77,17 +79,18 @@ def prep_search(query: str) -> str:
         max_tokens=100, # maximum number of tokens to generate
         n=1, # return only one completion
         stop=None, # stop at the end of the completion
-        top_p=0.95,
+        temperature=0.3, # more predictable
         stream=False # return the completion as a single string
     )
     search_query = completion.choices[0].message.content
     ### End of implementation
     return search_query
 
-def fetch_embedding(input: str) -> list[float]:
+def fetch_embedding(input: str, dimensions: int | NotGiven = NOT_GIVEN, model: Optional[str] = embeddings_deployment) -> list[float]:
     embedding = client.embeddings.create(
         input=input,
-        model="embedding", # "text-embedding-ada-002"
+        model=model,
+        dimensions=dimensions
     )
     return embedding.data[0].embedding
 
@@ -103,12 +106,23 @@ def search(req: func.HttpRequest) -> func.HttpResponse:
         )
 
     fts_query = prep_search(query)
-    if req.form.get('similarity-mode', 'similarity-processed') == 'similarity-processed':
-        embedding = fetch_embedding(fts_query)
-    else:
-        embedding = fetch_embedding(query)
 
-    sql_results = search_products(query, fts_query, embedding)
+    embedding_model = req.form.get('embedding-model', 'text-embedding-ada-002')
+    if embedding_model == 'text-embedding-ada-002':
+        dimensions = NOT_GIVEN
+        embedding_field = 'embedding'
+    elif embedding_model == 'text-embedding-3-large':
+        dimensions = 1024
+        embedding_field = 'embedding_large'
+    else:
+        raise ValueError("Invalid embedding model")
+
+    if req.form.get('similarity-mode', 'similarity-processed') == 'similarity-processed':
+        embedding = fetch_embedding(fts_query, model=embedding_model, dimensions=dimensions)
+    else:
+        embedding = fetch_embedding(query, model=embedding_model, dimensions=dimensions)
+
+    sql_results = search_products(query, fts_query, embedding, embedding_field)
 
     return func.HttpResponse(json.dumps({
         "keywords": fts_query,
@@ -124,7 +138,7 @@ def seed_embeddings(req: func.HttpRequest) -> func.HttpResponse:
     with open('data/test.json') as f:
         data = json.load(f)
         for product in data:
-            if product['embedding'] is None:
+            if 'embedding' not in product or product['embedding'] is None:
                 product['embedding'] = fetch_embedding(product['name'] + ' ' + product['description'])
 
         # Write the embeddings back to the test data
@@ -132,3 +146,19 @@ def seed_embeddings(req: func.HttpRequest) -> func.HttpResponse:
             json.dump(data, f)
                 
         return func.HttpResponse("Successfully seeded embeddings")
+
+@app.route(methods=["get"], auth_level="anonymous",
+           route="seed_embeddings_large")
+def seed_large_embeddings(req: func.HttpRequest) -> func.HttpResponse:
+    # Seed the embeddings for the products in the database by calling the OpenAI API
+    with open('data/test.json') as f:
+        data = json.load(f)
+        for product in data:
+            if 'embedding_large' not in product or product['embedding_large'] is None:
+                product['embedding_large'] = fetch_embedding(product['name'] + ' ' + product['description'], dimensions=1024, model="text-embedding-3-large")
+
+        # Write the embeddings back to the test data
+        with open('data/test.json', 'w') as f:
+            json.dump(data, f)
+                
+        return func.HttpResponse("Successfully seeded large embeddings")
